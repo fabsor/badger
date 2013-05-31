9bb8058262f473623abc1ca52aa71ac8dd7801a9
Fabian Sorqvist <fabian@sorqvist.nu>
2013-05-31 11:07:40 +0200

    Check that you can connect with new credentials

--- tests/test_mysql.py
+++ tests/test_mysql.py
@@ -27,8 +27,9 @@ class MysqlEngineTest(unittest.TestCase):
         platform = Platform("./testplatform", server)
         site = Site("example.com", platform, server)
         site.verify()
-        self.assertTrue(self.databaseExists("examplecom"))
-        self.assertTrue(self.userExists("examplecom", "127.0.0.1"))
+        self.assertTrue(self.databaseExists("examplecom"), "Database exists")
+        self.assertTrue(self.userExists("examplecom", "127.0.0.1"), "User exists")
+        self.assertTrue(self.connectionWorks(site), "Connection works")
 
     def userExists(self, user, host):
         db = mdb.connect("127.0.0.1", "root", "", "mysql")
@@ -47,6 +48,15 @@ class MysqlEngineTest(unittest.TestCase):
                 if row[0] == database:
                     return True
             return False
+
+    def connectionWorks(self, site):
+        try:
+            test_con = mdb.connect(site.database["host"], site.database["user"], site.database["password"], site.database["name"])
+            return True
+        except Exception:
+            return False
+
+
     def tearDown(self):
         db = mdb.connect("127.0.0.1", "root", "", "mysql")
         with db:
